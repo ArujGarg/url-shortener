c0a91ba3536042e851e319f540b4e6d8146229ce
ArujGarg <arujgarg04@gmail.com>
2026-05-27 00:18:39 +0530

    removed trailing slash in cors url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://url-shortener-eight-coral.vercel.app/",
+      "https://url-shortener-eight-coral.vercel.app",
     ],
   }),
 );
